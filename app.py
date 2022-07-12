4e1701fd9137d922018325c4ec2f95d3916ee215
Ch Hong <pnci1029@gmail.com>
2022-07-12 18:59:10 +0900

    no message

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from pymongo import MongoClient
 import jwt
 import datetime
@@ -8,30 +7,38 @@ from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
 
 
-=======
-from flask import Flask, render_template, jsonify, request
->>>>>>> 5c5337562d466ee38cc07b4c9b9564281ad0a434
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-<<<<<<< HEAD
 client = MongoClient('mongodb+srv://test:sparta123@cluster0.g2dpd.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
+
 @app.route('/')
 def main():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
     return render_template('index.html')
+
 
 @app.route('/register')
 def create():
     return render_template('new.html')
 
+
 @app.route('/edit')
 def edit():
     return render_template('edit.html')
+
 
 @app.route('/')
 def home():
@@ -64,15 +71,12 @@ def user(username):
         return render_template('user.html', user_info=user_info, status=status)
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
-    
-
-
 
 
 @app.route("/memo", methods=["GET"])
 def web_mars_get():
     car_list = list(db.cars.find({}, {'_id': False}))
-    return jsonify({'drcars':car_list})
+    return jsonify({'drcars': car_list})
 
 
 @app.route('/sign_in', methods=['POST'])
@@ -86,8 +90,8 @@ def sign_in():
 
     if result is not None:
         payload = {
-         'id': username_receive,
-         'exp': datetime.utcnow() + timedelta(seconds=60 * 60)  # 로그인 1시간 유지
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60)  # 로그인 1시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
         return jsonify({'result': 'success', 'token': token})
@@ -102,9 +106,9 @@ def sign_up():
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "username": username_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
@@ -115,33 +119,6 @@ def check_dup():
     username_receive = request.form['username_give']
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
-
-
-# @app.route('/update_profile', methods=['POST'])
-# def save_img():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         username = payload["id"]
-#         name_receive = request.form["name_give"]
-#         about_receive = request.form["about_give"]
-#         new_doc = {
-#             "profile_name": name_receive,
-#             "profile_info": about_receive
-#         }
-#         if 'file_give' in request.files:
-#             file = request.files["file_give"]
-#             filename = secure_filename(file.filename)
-#             extension = filename.split(".")[-1]
-#             file_path = f"profile_pics/{username}.{extension}"
-#             file.save("./static/"+file_path)
-#             new_doc["profile_pic"] = filename
-#             new_doc["profile_pic_real"] = file_path
-#         db.users.update_one({'username': payload['id']}, {'$set':new_doc})
-#         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
-
 
 
 
@@ -161,8 +138,9 @@ def get_posts():
         for post in posts:
             post["_id"] = str(post["_id"])
             post["count_heart"] = db.likes.count_documents({"post_id": post["_id"], "type": "thumbs-up"})
-            post["heart_by_me"] = bool(db.likes.find_one({"post_id": post["_id"], "type": "thumbs-up", "username": payload['id']}))
-        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.","cars":posts})
+            post["heart_by_me"] = bool(
+                db.likes.find_one({"post_id": post["_id"], "type": "thumbs-up", "username": payload['id']}))
+        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "cars": posts})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
@@ -191,8 +169,6 @@ def update_like():
         return redirect(url_for("home"))
 
 
-
-
 @app.route('/memo', methods=['POST'])
 def posting():
     token_receive = request.cookies.get('mytoken')
@@ -202,7 +178,6 @@ def posting():
         desc_receive = request.form["desc_give"]
         price_reveice = request.form["price_give"]
         file = request.files["file_give"]
-
 
         extension = file.filename.split('.')[-1]
 
@@ -256,73 +231,7 @@ def posting():
 #     db.cars.insert_one(doc)
 #
 #     return jsonify({'msg':'등록이 완료되었습니다!'})
-=======
-
-from pymongo import MongoClient
-import certifi
-
-ca = certifi.where()
-
-client = MongoClient('mongodb+srv://test:sparta@cluster0.a5dm1.mongodb.net/Cluster0?retryWrites=true&w=majority',
-                     tlsCAFile=ca)
-db = client.dbsparta
-
-
-from datetime import datetime
-
-## HTML을 주는 부분
-@app.route('/')
-def home():
-    return render_template('index2.html')
-
-
-@app.route('/register')
-def regi():
-    return render_template('index.html')
-
-
-## API 역할을 하는 부분
-@app.route('/memo', methods=['POST'])
-def saving():
-
-    desc_receive = request.form['desc_give']
-    price_receive = request.form['price_give']
-
-    file = request.files["file_give"]
-
-    extension = file.filename.split('.')[-1]
-
-    today = datetime.now()
-    mytime = today.strftime('%Y-%m-%H-%M-%S')
-
-    imagename = f'file-{mytime}'
-
-
-    save_to = f'static/{imagename}.{extension}'
-    file.save(save_to)
->>>>>>> 5c5337562d466ee38cc07b4c9b9564281ad0a434
-
-    doc = {
-
-        'desc':desc_receive,
-        'price':price_receive,
-        'file': f'{imagename}.{extension}'
-    }
-
-    db.cars.insert_one(doc)
-
-    return jsonify({'msg':'등록이 완료되었습니다!'})
-
-
-@app.route("/memo", methods=["GET"])
-def web_mars_get():
-    car_list = list(db.cars.find({}, {'_id': False}))
-    return jsonify({'drcars':car_list})
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run('0.0.0.0', port=5000, debug=True)
-=======
-   app.run('0.0.0.0',port=5000,debug=True)
->>>>>>> 5c5337562d466ee38cc07b4c9b9564281ad0a434
